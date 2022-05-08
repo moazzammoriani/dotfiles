d9c813f65c098378a5ef65acb483b21a3dd1b0a2
Moazzam Moriani <mozmoriani@gmail.com>
2022-05-08 19:26:12 +0500

    Refactor install script and add xinitrc

--- install.sh
+++ install.sh
@@ -3,15 +3,49 @@
 CURRENT_SCRIPT_DIR=$(dirname $0)
 
 if [[ $CURRENT_SCRIPT_DIR != "." ]]; then
-    DOT_FILES_DIR=$CURRENT_SCRIPT_DIR
+    DOTFILES_DIR=$CURRENT_SCRIPT_DIR
 else
-    DOT_FILES_DIR=$(pwd)
+    DOTFILES_DIR=$(pwd)
 fi
 
-for file in *; do
-    if [[ $file != "install.sh" ]] && \
-       [[ $file != "config" ]]; then
-        echo "$DOT_FILES_DIR/$file -> $HOME/.$file"
-        ln -s $DOT_FILES_DIR/$file $HOME/.$file
+SCAN_DIR=$DOTFILES_DIR
+FILE=""
+
+send_syms () {
+    if [[ $FILE != "install.sh" ]]; then
+        if [[ -e $SYM_LINK_PATH ]]; then
+            echo "Symbolic link for $FILE already exists"
+        else
+            echo "$DOTFILES_PATH -> $SYM_LINK"
+            ln -s $DOTFILES_PATH $SYM_LINK
+        fi
+    fi
+}
+
+for F in $SCAN_DIR/*; do
+    FILE=$(echo $F | sed "s/.*\///")
+    SYM_LINK_PATH=$HOME/.$FILE
+    echo $SYM_LINK_PATH
+    DOTFILES_PATH=$DOTFILES_DIR/$FILE
+    echo $DOTFILES_PATH
+
+    if [[ $FILE != "config" ]]; then 
+        send_syms
     fi
 done
+
+if [[ -e $DOTFILES_DIR/config ]]; then
+    SCAN_DIR=$DOTFILES_DIR/config
+    echo in config
+
+    for F in $SCAN_DIR/*; do
+        FILE=$(echo $F | sed "s/.*\///")
+        SYM_LINK_PATH=$HOME/.config/$FILE
+        echo $SYM_LINK_PATH
+        DOTFILES_PATH=$DOTFILES_DIR/config/$FILE
+        echo $DOTFILES_PATH
+
+        send_syms
+    done
+fi
+
